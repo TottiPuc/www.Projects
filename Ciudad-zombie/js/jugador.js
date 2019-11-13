b90d03aeab94b6237503a22f983b9ec089ab6a43
TottiPuc <dayan3846@gmail.com>
2019-11-13 16:45:02 -0500

    dando movimiento al jugador

---
+++ Ciudad-zombie/js/jugador.js
@@ -0,0 +1,45 @@
+/* El objeto jugador es un objeto literal que se encuentra incompleto.
+ Solo tiene asignadas algunas de sus propiedades y ningun metodo */
+var Jugador = {
+  /* el sprite contiene la ruta de la imagen
+  */
+  sprite: 'imagenes/auto_rojo_abajo.png',
+  x: 130,
+  y: 160,
+  ancho: 15,
+  alto: 30,
+  velocidad: 10,
+  vidas: 5,
+  mover:function(movX,movY){
+    this.x = this.x + movX;
+    this.y = this.y +movY;
+    // cambiar la direccion del jugador
+      if (movX<0) {
+          this.sprite= 'imagenes/auto_rojo_izquierda.png';
+          this.ancho= 30;
+          this.alto= 15;
+      }
+        if (movX>0) {
+          this.sprite= 'imagenes/auto_rojo_derecha.png';
+          this.ancho= 30;
+          this.alto= 15;
+      }
+        if (movY<0) {
+          this.sprite= 'imagenes/auto_rojo_arriba.png';
+          this.ancho= 15;
+          this.alto= 30;
+      }
+      if (movY>0) {
+        this.sprite= 'imagenes/auto_rojo_abajo.png';
+        this.ancho= 15;
+        this.alto= 30;
+      }
+  },
+  perderVidas:function(numVidas) {
+    this.vidas = this.vidas - numVidas;
+    console.log("me restan "+ this.vidas) 
+  }
+  // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
+  // y todo lo que haga falta para que cumpla con sus responsabilidades
+
+}
